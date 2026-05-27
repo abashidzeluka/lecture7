e186624f1281cc1a78e680e78f88b28aa8e5e87d
abashidzeluka <abashidze2003@gmail.com>
2026-05-27 17:44:05 +0400

    Initial commit

---
+++ Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product
+            {
+                Name = "Laptop",
+                Brand = "Lenovo",
+                Price = 2500,
+                Category = "Computers",
+                Description = "A lightweight, ultra-portable",
+                Id = 45513,
+                Rating = 4.5f,
+                Quantity = 2000,
+            };
+
+            
+
+            product.AddQuantity(4);
+            product.Discount(10);
+
+            product.DisplayInfo();
+
+        }
+    }
+}
---
+++ Assignment/product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Category { get; set; }
+
+        public float Rating { get; set; }
+
+        public bool IsAvailable => this.Quantity > 0;
+
+        public decimal DiscountPercent { get; set; }
+
+        public decimal DiscountedPrice { get; set; }
+
+
+        public void Discount(byte discountPercent)
+        {
+            if (discountPercent > 100)
+            {
+               Console.WriteLine("Discount can't exceed 100%");
+                return;
+            }
+            this.DiscountPercent = discountPercent;
+            this.DiscountedPrice = this.Price - this.Price * discountPercent / 100;
+        }
+
+        public void AddQuantity (int quantity)
+        {
+            this.Quantity += quantity;
+        }
+
+        public void DisplayInfo ()
+        {
+            Console.WriteLine("Product: " + this.Name);
+            Console.WriteLine("Price: " + this.Price);
+            Console.WriteLine($"Discount: {this.DiscountPercent}%");
+            Console.WriteLine($"Last Price: {this.DiscountedPrice}$");
+            Console.WriteLine("In Stock: " + this.IsAvailable);
+        }
+
+
+    }
+}
